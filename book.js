d7467bdc5381e5199e431a166e8a981e80a36d46
devSudhir <sudhirsamantaray33@gmail.com>
2024-12-24 01:16:19 +0530

    validation error test cases

--- book.js
+++ book.js
@@ -1,23 +1,23 @@
 let books = [
   {
     id: 1,
-    title: '1984',
-    author: 'Gorge',
+    title: "1984",
+    author: "Gorge",
   },
   {
     id: 2,
-    title: 'The great gatsby',
-    author: 'F. Scott Fitzgerald',
+    title: "The great gatsby",
+    author: "F. Scott Fitzgerald",
   },
   {
     id: 3,
-    title: 'Pride and Prejustice',
-    author: 'Jane Austen',
+    title: "Pride and Prejustice",
+    author: "Jane Austen",
   },
   {
     id: 4,
-    title: 'To Kill a Mockingbird',
-    author: 'Harper Lee',
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
   },
 ];
 
@@ -30,7 +30,6 @@ async function getBookById(bookId) {
 }
 
 async function addBook(book) {
-  console.log(book);
   let newBook = {
     id: books.length + 1,
     ...book,
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,1 +1,49 @@
-const { getAllBooks, getBookById, addBook } = require('../book.js');
+const { getAllBooks, getBookById, addBook } = require("../book.js");
+const { app, validateBook } = require("../index.js");
+const http = require("http");
+const request = require("supertest");
+
+jest.mock("../index.js", () => ({
+  ...jest.requireActual("../index.js"),
+  validateBook: jest.fn(),
+}));
+
+let server;
+beforeAll((done) => {
+  server = http.createServer(app);
+  server.listen(3002, done);
+});
+
+afterAll((done) => {
+  server.close(done);
+});
+
+describe("API TEST", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  test("create new book test", async () => {
+    const mockBook = {
+      title: "1996",
+      author: "sudhir",
+    };
+
+    const result = await request(server).post("/api/book/new").send(mockBook);
+    expect(result.status).toBe(201);
+    expect(result.body).toEqual({
+      id: 5,
+      title: "1996",
+      author: "sudhir",
+    });
+  });
+
+  test("Throw error message if wrong data provided", async () => {
+    const mockBook = {
+      title: 1996,
+      author: "sudhir",
+    };
+    const result = await request(server).post("/api/book/new").send(mockBook);
+    expect(result.status).toBe(400);
+    expect(result.body.error).toEqual("Book title is invalid!");
+  });
+});
